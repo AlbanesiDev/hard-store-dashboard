7561195780205480ee82cfff4a9e2bae9cbdb4dd
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-05-09 23:05:13 -0300

    update config: The provideFirestore() has been added

--- src/app/core/config/firebase.config.ts
+++ src/app/core/config/firebase.config.ts
@@ -5,12 +5,14 @@ import { getStorage, provideStorage } from "@angular/fire/storage";
 //auth
 import { getAuth, provideAuth } from "@angular/fire/auth";
 // Environments
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from "../../../environments/environment";
 
 const provideFirebase: EnvironmentProviders = importProvidersFrom(
   provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
   provideAuth(() => getAuth()),
   provideStorage(() => getStorage()),
+  provideFirestore(() => getFirestore()),
 );
 
 export { provideFirebase };
